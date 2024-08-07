11ac002e5fe332a2abeba1774db5eb30912ca2cb
thatriple-e <148991076+thatriple-e@users.noreply.github.com>
2024-08-07 02:55:04 +0100

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,7 @@
+global.prefa = ['','!','.',',','🐤','🗿']
+
+global.owner = ['62xxxxxxx']
+global.botname = 'ClaraBotz'
+global.baileys1 = require('@whiskeysockets/baileys') 
+global.sticker1 = "Clara Official"
+global.sticker2 = "🌜"
